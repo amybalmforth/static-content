125d80054a75b4a52ae870265758fb765c538ffe
Amy Balmforth <amy.balmforth@gmail.com>
2020-07-11 14:02:48 +0100

    grab the route url and read relevant md file

--- app.js
+++ app.js
@@ -1,22 +1,30 @@
 const express = require('express');
-const path = require('path')
+const path = require('path');
+const fs = require('fs');
 const app = express();
 const port = 3000;
+const markdown = require('markdown-it')();
 
+const result = markdown.render('# mark down it rules');
 
-app.get('/about-page', (req, res, next) => {
+app.get('/about-page', (req, res) => {
+  let directory = req.url
+  let url = `content${directory}/index.md`
+  fs.readFile(url, 'utf8', (err, data) => {
+    console.log(data)
+  })
+  res.send(result)
+})
+
+app.get('/jobs', (req, res) => {
   res.sendFile('template.html', { root: '.' })
 })
 
-app.get('/jobs', (req, res, next) => {
+app.get('/valves', (req, res) => {
   res.sendFile('template.html', { root: '.' })
 })
 
-app.get('/valves', (req, res, next) => {
-  res.sendFile('template.html', { root: '.' })
-})
-
-app.use((req, res, next) => {
+app.use((req, res) => {
   res.status(404).send({
     status: 404,
     error: 'Something went wrong'
